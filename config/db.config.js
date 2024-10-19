95b0f0f1f5494fa1f0d77173df9e0e51eaf74fea
sanjayan100 <161213517+sanjayan100@users.noreply.github.com>
2024-10-20 01:55:39 +0530

    Update db.config.js

--- config/db.config.js
+++ config/db.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     host: 'localhost',
-    user: 'root',
-    password: 'root@123',
-    database: 'ecommerce_db',
+    user: 'user',
+    password: 'password',
+    database: 'db_name',
     port: 3306,
 };
